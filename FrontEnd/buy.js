21d86c80f6814a2853ee83e03ef55b521657d518
김은수 <03robin@naver.com>
2023-11-17 04:01:03 +0900

    추가 : 모든 화면에서 로그아웃 누르면 로그인화면으로 가도록

--- FrontEnd/buy.js
+++ FrontEnd/buy.js
@@ -11,3 +11,17 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    // DOM이 완전히 로드된 후에 실행되는 코드
+
+    // 로그아웃 텍스트를 클릭하면 login.html로 이동
+    var logoutText = document.getElementById('logout');
+
+    if (logoutText) {
+        logoutText.addEventListener('click', function () {
+            // login.html로 이동
+            window.location.href = 'login.html';
+        });
+    }
+});
