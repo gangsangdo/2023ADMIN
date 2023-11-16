5caa7ef54d52edd5467dc1161b86043e091d0c20
김은수 <03robin@naver.com>
2023-11-17 03:53:29 +0900

    추가 : 종 모양 > 알람 페이지 연결

--- FrontEnd/itemFind.js
+++ FrontEnd/itemFind.js
@@ -49,3 +49,17 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    // DOM이 완전히 로드된 후에 실행되는 코드
+
+    // 알림 이미지를 클릭하면 alarm.html로 이동
+    var notificationImage = document.getElementById('notification');
+
+    if (notificationImage) {
+        notificationImage.addEventListener('click', function () {
+            // alarm.html로 이동
+            window.location.href = 'alram.html';
+        });
+    }
+});
